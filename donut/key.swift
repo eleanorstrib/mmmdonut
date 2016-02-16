34fa85c7aa8da25d82f75541e95bd224def91653
eleanorstrib <eleanor@sloan.mit.edu>
2016-02-16 05:33:31 -0800

    added flickr keys to file, .gitignore

---
+++ donut/key.swift
@@ -0,0 +1,12 @@
+//
+//  key.swift
+//  donut
+//
+//  Created by Eleanor Stribling on 2/16/16.
+//  Copyright © 2016 eleanorstrib. All rights reserved.
+//
+
+import Foundation
+
+let FLICKR_API_KEY="6974e22b5bbe37ca1e61632fbe116d07"
+let FLICKR_API_SECRET="b32e66b41c08ddd5"
