e61b0f0df5ac0af8810df6af2b067fa340833fd7
eleanorstrib <eleanor@sloan.mit.edu>
2016-02-16 05:24:31 -0800

    recreated files - unknown bug not permitting me to move storyboard elements around on the canvas

---
+++ donut/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  donut
+//
+//  Created by Eleanor Stribling on 2/16/16.
+//  Copyright © 2016 eleanorstrib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var flickrImage: UIImageView!
+    @IBOutlet weak var moreDonuts: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ donutTests/donutTests.swift
@@ -0,0 +1,36 @@
+//
+//  donutTests.swift
+//  donutTests
+//
+//  Created by Eleanor Stribling on 2/16/16.
+//  Copyright © 2016 eleanorstrib. All rights reserved.
+//
+
+import XCTest
+@testable import donut
+
+class donutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
